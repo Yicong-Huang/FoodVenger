78df76ca54f63cf2b13ccec0eca7a1ed4c45f008
Yicong-Huang <hyc541978023@gmail.com>
2018-01-22 23:43:34 -0800

    project 1

--- src/FoodListServlet.java
+++ src/FoodListServlet.java
@@ -4,14 +4,97 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
 
-@WebServlet(name = "FoodListServlet")
+
+@WebServlet(name = "FoodListServlet",
+        urlPatterns = {"/FoodList"})
+
+
 public class FoodListServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException {
+        //SQL Setup Connection Info
+        String loginUser = "root";
+        String loginPassword = "950929huang";
+        String loginUrl = "jdbc:mysql://localhost:3306/restdb";
+
+        //Content type
+        response.setContentType("text/html");
+
+
+        //Output stream to webpage
+        PrintWriter out = response.getWriter();
+
+        //CSS
+        out.println("<head>"
+                + "<title>Top 20 Rating Restaurants</title>"
+                + "<style>"
+                + "td {"
+                + "	padding:20px"
+                + "}"
+                + "</style>"
+                + "</head>");
+
+        //Body
+        out.println("<body>");
+
+        //Title
+        out.println("<h2>Top 20 Rating Restaurants</h2>");
+
+        try {
+            //SQL Setup JDBC Connection
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection dbcon = DriverManager.getConnection(loginUrl, loginUser, loginPassword);
+
+            //SQL statement object and query
+            Statement statement = dbcon.createStatement();
+            String query = "SELECT  rs.name, rs.addr, c.type AS cuisine, d.name AS dish, r.rating FROM restdb.restuarants AS rs, cuisine AS c,cuisine_in_restau AS cr, ratings AS r , dishes AS d, dishes_in_restau AS dr WHERE d.id = dr.did AND dr.rid = rs. id AND rs.id = cr.rid AND cr.cid = c.id AND  r.rid = rs.id ORDER BY r.rating DESC LIMIT 20;";
+            ResultSet resultSet = statement.executeQuery(query);
+            out.println("<TABLE border>");
+            out.println("<thead><tr><th>Restaurant</th><th>Address</th><th>Cuisine</th>"
+                    + "<th>Dish List</th><th>Ratings</th></tr></thead>");
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String addr = resultSet.getString("addr");
+                String cuisine = resultSet.getString("cuisine");
+                String dish = resultSet.getString("dish");
+                String rating = resultSet.getString("rating");
+
+                out.println("<tr style='border: 15px solid gray'>"
+                        + "<td>" + name + "</td>"
+                        + "<td>" + addr + "</td>"
+                        + "<td>" + cuisine + "</td>"
+                        + "<td>" + dish + "</td>"
+                        + "<td>" + rating + "</td>"
+                        + "</tr>");
+
+            }
+
+            out.println("</TABLE>");
+            resultSet.close();
+
+
+            statement.close();
+            dbcon.close();
+
+
+        } catch (SQLException e) {
+            while (e != null) {
+                e = e.getNextException();
+            }
+        } catch (java.lang.Exception e) {
+            out.println("<HTML>" + "<HEAD><TITLE>" + "WEB ERROR" + "</TITLE></HEAD>\n</BODY></HTML>");
+            return;
+        }
+        out.println("</body>");
+        out.close();
+
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
 }
